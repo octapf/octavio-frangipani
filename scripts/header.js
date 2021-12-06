78f27e61c909e0afdf8d8b9d8494e35a8b3fdbc6
Octavio Frangipani <frangipani.octavio@gmail.com>
2021-12-05 22:45:36 -0300

    fix mobile front

--- scripts/header.js
+++ scripts/header.js
@@ -3,7 +3,7 @@ const header = document.createElement('header')
 header.innerHTML = `<header id="header" class="mar-bottom1">
 <nav>
     <div class="navbar-flex bgc-petrol">
-    <a class="logo" href="index.html"><h1>Octavio Frangipani</h1></a>
+    <a class="logo" href="index.html"><h1>Octavio Frangipani</h1><small class="sublogo">Web Developer</small></a>
         <ul class="ul-flex" >
             <a class="pale" href="index.html"><li>Home</li></a>
             <a class="pale" href="cv.html"><li>CV</li></a>
