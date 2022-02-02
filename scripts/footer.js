f6859877e4da0e490342027e53caa50ad286d6f9
Octavio Frangipani <62032399+octapf@users.noreply.github.com>
2022-02-02 17:24:30 +0100

    add worksut experience

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,13 +1,13 @@
 const footer = document.createElement('footer')
 
-footer.innerHTML = `<footer id="footer" class="footer text-centered bgc-petrol sky">
-                    <a class="sky mar1" href="#header"><em class="fas fa-chevron-up"></em></a>
+footer.innerHTML = `<footer id="footer" class="footer text-centered bgc-petrol">
+                    <a class="pale mar1" href="#header"><em class="fas fa-chevron-up"></em></a>
                     <section class="flex justify-content-center">
-                    <a class="sky supertitle4 mar1" href="https://www.linkedin.com/in/octavio-frangipani-080aa947/" target="_blank"><em class="fab fa-linkedin"></em></a>
-                    <a class="sky supertitle4 mar1" href="https://github.com/octapf?tab=repositories" target="_blank"><em class="fab fa-github-square"></em></a>
-                    <a class="sky supertitle4 mar1" href="https://wa.me/34722696514" target="_blank"><em class="fab fa-whatsapp-square"></em></a>
-                    <a class="sky supertitle4 mar1" href="mailto:frangipani.octavio@gmail.com"><em class="fas fa-envelope-square"></em></a></section>
-                    <p class="copyright">©Copyright Octavio Frangipani</p>
+                    <a class="pale supertitle4 mar1" href="https://www.linkedin.com/in/octavio-frangipani-080aa947/" target="_blank"><em class="fab fa-linkedin-in"></em></a>
+                    <a class="pale supertitle4 mar1" href="https://github.com/octapf?tab=repositories" target="_blank"><em class="fab fa-github"></em></a>
+                    <a class="pale supertitle4 mar1" href="https://wa.me/34722696514" target="_blank"><em class="fab fa-whatsapp"></em></a>
+                    <a class="pale supertitle4 mar1" href="mailto:frangipani.octavio@gmail.com"><em class="fas fa-envelope"></em></a></section>
+                    <p class="pale copyright">©Copyright Octavio Frangipani</p>
                     </footer>`
 
 document.body.append(footer)
--- scripts/header.js
+++ scripts/header.js
@@ -7,10 +7,10 @@ header.innerHTML = `<header id="header" class="mar-bottom1">
         <ul class="ul-flex" >
             <a class="pale" href="index.html"><li>Home</li></a>
             <a class="pale" href="cv.html"><li>CV</li></a>
-            <a class="pale" href="box-model.html"><li>Box-Model</li></a>
+            
             <a class="pale" href="notes.html"><li>Notes</li></a>
-            <a class="pale" href="tech-news.html"><li>TechNews</li></a>
-            <a class="pale" href="pictures-grid.html"><li>Pictures-Grid</li></a>
+            
+            
             <a class="pale" href="https://proshop-app-177.herokuapp.com/"><li>Proshop</li></a>
         </ul>
     </div>
